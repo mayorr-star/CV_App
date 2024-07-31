50b4f7d83f7f05f03f6b7d1ea875a6586fc1f47b
mayorr-star <immaboat11@gmail.com>
2024-07-31 14:32:56 +0000

    Add props to the textarea component
    To pass data from its parent

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable react/prop-types */
-const Input = ({ type = "text", name, id = name, text }) => {
+const Input = ({ type = "text", name, id = name, text, handlechange, value }) => {
   return (
     <>
       <label htmlFor={id}>{text}</label>
-      <input type={type} name={name} id={id} />
+      <input type={type} name={name} id={id} onChange={(e) => handlechange(e)} value={value}/>
     </>
   );
 };
 export default Input;
 
-const TextArea = () => {
+const TextArea = ({text, id, handlechange}) => {
   return (
     <>
-      <label htmlFor="text_area">Professional Statement:</label>
-      <textarea rows={5} cols={20} id="text_area"></textarea>
+      <label htmlFor="text_area">{text}</label>
+      <textarea rows={5} cols={20} id={id} onChange={handlechange}></textarea>
     </>
   );
 };
