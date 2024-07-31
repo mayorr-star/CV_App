39ae405967fcac8340b50a1f9f7984ffb3b76bf5
mayorr-star <immaboat11@gmail.com>
2024-07-31 20:29:26 +0000

    Render the work history form
    To collect users work history

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,13 +3,35 @@ import GeneralInfoForm from "./generalInfoForm";
 import EducationalExpForm from "./educationalExpForm";
 import WorkExperienceForm from "./WorkExperience";
 
-const Form = ({ handleGeneralInfo, handleEducationInfo, handleWorkInfo, generalInfo, educationInfo, workInfo}) => {
-  
+const Form = ({
+  handleGeneralInfo,
+  handleEducationInfo,
+  handleWorkInfo,
+  generalInfo,
+  educationInfo,
+  workInfo,
+}) => {
   return (
     <form action="" method="">
-      <GeneralInfoForm handlechange={handleGeneralInfo} data={generalInfo}/>
-      <EducationalExpForm handleChange={handleEducationInfo} data={educationInfo}/>
-      <WorkExperienceForm handleChange={handleWorkInfo} data={workInfo}/>
+      <GeneralInfoForm handlechange={handleGeneralInfo} data={generalInfo} />
+      {educationInfo.map((info) => {
+        return (
+          <EducationalExpForm
+            key={info.id}
+            handleChange={handleEducationInfo}
+            keyId={info.id}
+          />
+        );
+      })}
+      {workInfo.map((info) => {
+        return (
+          <WorkExperienceForm
+            key={info.id}
+            handleChange={handleWorkInfo}
+            keyId={info.id}
+          />
+        );
+      })}
     </form>
   );
 };
