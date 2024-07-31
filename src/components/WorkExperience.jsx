a89d764a7091dddcdcb41e9b3ed9570b9c7702f1
mayorr-star <immaboat11@gmail.com>
2024-07-31 20:27:33 +0000

    Give the fieldest element a dataset attribute
    To match the unique ids of elements in the workInfo array

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -1,15 +1,16 @@
+/* eslint-disable react/prop-types */
 import Button from "./button";
 import Input, { TextArea } from "./input";
 
-const WorkExperienceForm = () => {
+const WorkExperienceForm = ({handleChange, keyId}) => {
   return (
-    <fieldset>
+    <fieldset data-key={keyId}>
       <legend>Work History</legend>
       <div className="form_controls">
-        <Input name="companyName" text="Company Name:" />
-        <Input name="postion" text="Position Title:" />
-        <TextArea />
-        <Input name="duration" text="Duration:" />
+        <Input name="companyName" text="Company Name:" handlechange={handleChange}/>
+        <Input name="position" text="Position Title:" handlechange={handleChange}/>
+        <TextArea text="Main responsibilites:" id="roles"/>
+        <Input name="duration" text="Duration:" handlechange={handleChange}/>
       </div>
       <div className="button_container">
         <Button text="Edit" />
