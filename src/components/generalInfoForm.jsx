1931baec380225aadad0fc6a4db935b46d8e59a9
mayorr-star <immaboat11@gmail.com>
2024-07-31 14:36:25 +0000

    Add a textarea component
    Users can can input a brief summary of the themselves

--- src/components/generalInfoForm.jsx
+++ src/components/generalInfoForm.jsx
@@ -1,17 +1,52 @@
-import Input, { TextArea } from "./input"
+/* eslint-disable react/prop-types */
+import Input, { TextArea } from "./input";
+import Button from "./button";
 
-const GeneralInfoForm = () => {
-    return (
-        <fieldset>
-            <legend>General Information</legend>
-            <div className="form_controls">
-                <Input type="text" name="firstName" text="First Name:"/>
-                <Input type="text" name="lastName" text="Last Name:"/>
-                <Input type="tel" name="phoneNumber" text="Phone Number:"/>
-                <Input type="email" name="email" text="Email:"/>
-                <TextArea/>
-            </div>
-        </fieldset>
-    )
-}
-export default GeneralInfoForm
+const GeneralInfoForm = ({ handlechange, data }) => {
+  const { firstName, lastName, phoneNumber, email, statement } = data;
+
+  return (
+    <fieldset>
+      <legend>General Information</legend>
+      <div className="form_controls">
+        <Input
+          name="firstName"
+          text="First Name:"
+          handlechange={handlechange}
+          value={firstName}
+        />
+        <Input
+          name="lastName"
+          text="Last Name:"
+          handlechange={handlechange}
+          value={lastName}
+        />
+        <Input
+          type="tel"
+          name="phoneNumber"
+          text="Phone Number:"
+          handlechange={handlechange}
+          value={phoneNumber}
+        />
+        <Input
+          type="email"
+          name="email"
+          text="Email:"
+          handlechange={handlechange}
+          value={email}
+        />
+        <TextArea
+          text="Professional Statement:"
+          id="statement"
+          handlechange={handlechange}
+          value={statement}
+        />
+        <div className="button_container">
+          <Button text="Edit" />
+          <Button text="Confirm" />
+        </div>
+      </div>
+    </fieldset>
+  );
+};
+export default GeneralInfoForm;
