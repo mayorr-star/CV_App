968e3aafd39a4c3ee8854d86f6604ff8da5be6ec
mayorr-star <immaboat11@gmail.com>
2024-07-31 14:37:33 +0000

    Create an object
    Store data of users

---
+++ src/components/data.js
@@ -0,0 +1,27 @@
+const data = {
+  generalInfo: {
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    email: "",
+    statement: "",
+  },
+  educationInfo: [
+    {
+      id: "",
+      schoolName: "",
+      program: "",
+      duration: "",
+    },
+  ],
+  workInfo: [
+    {
+      id: "",
+      companyName: "",
+      position: "",
+      duration: "",
+      roles: "",
+    },
+  ],
+};
+export default data;
