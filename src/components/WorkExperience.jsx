e4aa2a25e96521852eed4a5225fce472642ba479
mayorr-star <immaboat11@gmail.com>
2024-08-01 10:52:41 +0000

    Change the place of the textarea element
    Increase accessibility

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -12,7 +12,7 @@ const WorkExperienceForm = ({handleChange, keyId}) => {
       <div className="form_controls">
         <Input name="companyName" text="Company Name:" handlechange={handleChange} placeholder="Company Name" isActive={activeIndex === 1}/>
         <Input name="position" text="Position Title:" handlechange={handleChange} placeholder="e.g. Manager" isActive={activeIndex === 1}/>
-        <TextArea text="Main responsibilites:" id="roles" placeholder="Taking minutes." isActive={activeIndex === 1}/>
+        <TextArea text="Main responsibilites:" id="roles" placeholder="List your responsibilites here." isActive={activeIndex === 1} handlechange={handleChange}/>
         <Input name="duration" text="Duration:" handlechange={handleChange} placeholder="Sept, 2019 - Jun, 2024" isActive={activeIndex === 1}/>
       </div>
       <div className="button_container">
