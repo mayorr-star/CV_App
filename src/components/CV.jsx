4e01cc2fe1d32938c400420a4e1c4d8677b19e9b
mayorr-star <immaboat11@gmail.com>
2024-08-01 10:56:36 +0000

    Generate information dynamically from data passed a s props

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,37 +1,55 @@
 /* eslint-disable react/prop-types */
-export default function CV({generalInfo, educationInfo, workInfo}) {
+export default function CV({ generalInfo, educationInfo, workInfo }) {
   return (
-      <div className="CV">
-        <div className="main_content">
-          <header>
-            <h2>{generalInfo.firstName + " " + generalInfo.lastName}</h2>
-            <hr />
-            <h3>PROFESSIONAL SUMMARY</h3>
-            <hr />
-            <p>{generalInfo.statement}</p>
-          </header>
-          <section className="work_history">
-            <h3>WORK HISTORY</h3>
-            <hr />
-            <span>Position</span>
-            <span>Duration</span>
-            <span>Company</span>
-            <ul>roles</ul>
-          </section>
-        </div>
-        <div className="aside">
-          <section className="contacts">
-            <span>{generalInfo.email}</span>
-            <span>{generalInfo.phoneNumber}</span>
-          </section>
-          <section className="education">
-            <div className="education_content">
-              <span>Program</span>
-              <span>Duration</span>
-              <span>School Name</span>
-            </div>
-          </section>
-        </div>
+    <div className="CV">
+      <div className="main_content">
+        <header>
+          <h2>{generalInfo.firstName + " " + generalInfo.lastName}</h2>
+          <hr />
+          <h3>PROFESSIONAL SUMMARY</h3>
+          <hr />
+          <p>{generalInfo.statement}</p>
+        </header>
+        <section className="work_history">
+          <h3>WORK HISTORY</h3>
+          <hr />
+          <ul>
+            {workInfo.map((info) => {
+              return (
+                <li key={info.id}>
+                  <span>{info.position}</span>
+                  <span>{info.duration}</span>
+                  <span>{info.companyName}</span>
+                  <ul>
+                    {info.roles.map((role, index) => <li key={index}>{role}</li>)}
+                  </ul>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
       </div>
+      <div className="aside">
+        <section className="contacts">
+          <span>{generalInfo.email}</span>
+          <span>{generalInfo.phoneNumber}</span>
+        </section>
+        <section className="education">
+          <ul>
+            {educationInfo.map((info) => {
+              return (
+                <li key={info.id}>
+                  <div className="education_content">
+                    <span>{info.program}</span>
+                    <span>{info.duration}</span>
+                    <span>{info.schoolName}</span>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+      </div>
+    </div>
   );
 }
