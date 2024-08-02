a29571d5b76984ae46eaba84b6c3be2c3f52be6b
mayorr-star <immaboat11@gmail.com>
2024-08-02 22:23:47 +0000

    Create function to toggle form fields

--- src/modules/helper.js
+++ src/modules/helper.js
@@ -1,13 +1,13 @@
-const renderBtns = document.querySelectorAll("render_btn");
-const toggleForms = () => {
-    renderBtns.forEach(button => {
-        const formFields = document.querySelectorAll(".hide");
-        formFields.forEach(formField => {
-            if (button.classList.contains("general") && formField.classList.contains("general")) {
-                formField.classList.toggle("show")
-            }
-        })
-        
-    })
-}
+const toggleForms = (e) => {
+      if (e.target.classList.contains("general")) {
+        const general = document.querySelector("fieldset.general");
+        general.classList.toggle("show");
+      } else if (e.target.classList.contains("edu")) {
+        const edu = document.querySelector(".education_section");
+        edu.classList.toggle("show");
+      } else {
+        const work = document.querySelector(".work_section");
+        work.classList.toggle("show");
+      }
+};
 export default toggleForms;
