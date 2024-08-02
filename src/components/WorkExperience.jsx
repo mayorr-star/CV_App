1c97ab4cbc06520c30ec3f84d74f5fa67958c2c4
mayorr-star <immaboat11@gmail.com>
2024-08-02 20:38:06 +0000

    Give each input a unique id
    To connect to their corresponding labels

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -11,10 +11,10 @@ const WorkExperienceForm = ({handleChange, keyId, validate}) => {
     <fieldset data-key={keyId}>
       <legend>Work History</legend>
       <div className="form_controls">
-        <Input name="companyName" text="Company Name:" handlechange={handleChange} placeholder="Company Name" isActive={activeIndex === 1}/>
-        <Input name="position" text="Position Title:" handlechange={handleChange} placeholder="e.g. Manager" isActive={activeIndex === 1}/>
-        <TextArea text="Main responsibilites:" id="roles" placeholder="List your responsibilites here." isActive={activeIndex === 1} handlechange={handleChange}/>
-        <Input name="duration" text="Duration:" handlechange={handleChange} placeholder="Sept, 2019 - Jun, 2024" isActive={activeIndex === 1}/>
+        <Input name="companyName" text="Company Name:" handlechange={handleChange} placeholder="Company Name" isActive={activeIndex === 1} id={"companyName" + keyId}/>
+        <Input name="position" text="Position Title:" handlechange={handleChange} placeholder="e.g. Manager" isActive={activeIndex === 1} id={"position" + keyId}/>
+        <TextArea text="Main responsibilites:" id="roles" placeholder="List your responsibilites here." isActive={activeIndex === 1} handlechange={handleChange} dataId="roles"/>
+        <Input name="duration" text="Duration:" handlechange={handleChange} placeholder="Sept, 2019 - Jun, 2024" isActive={activeIndex === 1} id={"duration" + keyId}/>
       </div>
       <div className="button_container">
         <Button text="Edit" isActive={activeIndex === 0} handleClick={() => setActiveIndex(1)}/>
--- src/components/educationalExpForm.jsx
+++ src/components/educationalExpForm.jsx
@@ -10,9 +10,9 @@ const EducationalExpForm = ({handleChange, keyId, validate}) => {
         <fieldset data-key={keyId}>
             <legend>Education</legend>
             <div className="form_controls">
-                <Input name="schoolName" text="School Name:" handlechange={handleChange} placeholder="e.g University of Ghana" isActive={activeIndex === 1}/>
-                <Input name="program" text="Title of Study:" handlechange={handleChange} placeholder="e.g BSc Mechanical Engineering" isActive={activeIndex === 1}/>
-                <Input name="duration" text="Date of Study:" handlechange={handleChange} placeholder="Sept, 2019 - Aug, 2023" isActive={activeIndex === 1}/>
+                <Input name="schoolName" text="School Name:" handlechange={handleChange} placeholder="e.g University of Ghana" isActive={activeIndex === 1} id={"schoolName" + keyId}/>
+                <Input name="program" text="Title of Study:" handlechange={handleChange} placeholder="e.g BSc Mechanical Engineering" isActive={activeIndex === 1} id={"program" + keyId}/>
+                <Input name="duration" text="Date of Study:" handlechange={handleChange} placeholder="Sept, 2019 - Aug, 2023" isActive={activeIndex === 1} id={"duration" + keyId}/>
             </div>
             <div className="button_container">
             <Button text="Edit" isActive={activeIndex === 0} handleClick={() => setActiveIndex(1)}/>
